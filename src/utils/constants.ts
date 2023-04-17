c65bc8e23a6d5a052bb354873de56a2bed2ddbf2
sixbeeshades <abhijith.p.subash@gmail.com>
2023-04-17 13:16:00 +0530

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,5 @@ export const developerName = "Abhijith P Subash";
 export const gitRepoLink = "https://github.com/yourcompany/your-repo";
 export const linkedinLink =
   "https://www.linkedin.com/in/abhijith-p-subash-the-engineer/";
+
+export const website = "https://abhijithpsubash.com";
--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -4,14 +4,15 @@ import {
   developerName,
   gitRepoLink,
   linkedinLink,
+  website,
 } from "./constants.js";
 
 export function Info() {
   return `${chalk.green("Version")}: ${chalk.red("0.0.1")} \n${chalk.green(
     "Developer"
-  )}: ${chalk.red(developerName)} \n ${chalk.green("Git Repo")}: ${chalk.red(
-    gitRepoLink
-  )} \n ${chalk.green("Linkedin")}: ${chalk.red(linkedinLink)}`;
+  )}: ${chalk.red(developerName)}\n ${chalk.green("Linkedin")}: ${chalk.red(
+    linkedinLink
+  )}\n${chalk.green("Website")}: ${chalk.red(website)}\n`;
 }
 
 export function delay(ms: number): Promise<void> {
